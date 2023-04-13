1c4b78df95504681b7fba1aac8350b6ec1676ac0
Whoami354 <fabian.gregory98@hotmail.de>
2023-04-13 10:20:13 +0200

    Tag 4 Teil 01 angepasst

--- Day04/part01.py
+++ Day04/part01.py
@@ -1,18 +1,16 @@
+from collections import defaultdict
 lines = open("input", "r", encoding='utf-8').read().strip().split('\n')
 sumRealRooms = 0
 
 for line in lines:
-    letters = {}
+    letters = defaultdict(int)
     checkLastLetters = ""
     words = line.split('-')
     lastValues = words[-1]
     words.pop()
     for word in words:
         for letter in word:
-            if not letter in letters:
-                letters[letter] = 1
-            else:
-                letters[letter] += 1
+            letters[letter] += 1
         sortedLetters = sorted(letters.items(), key=lambda x:(-x[1],x[0]))[:5]
         checkTheLetters = [b[0] for b in sortedLetters]
         values = lastValues.split('[')
