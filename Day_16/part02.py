496a4da9cafcddd4226a8fea2547adf821180ef9
Whoami354 <fabian.gregory98@hotmail.de>
2023-05-05 21:26:35 +0200

    Tag 16 gemacht

--- Day_16/part02.py
+++ Day_16/part02.py
@@ -1,7 +1,7 @@
 input = "00111101111101000"
 disk_size = 35651584
 
-def dragon_cuve(data, size):
+def dragon_curve(data, size):
     while len(data) < size:
         a = data
         b = a[::-1].translate(str.maketrans("01", "10"))
@@ -13,6 +13,6 @@ def checksum(data):
         data = ''.join(['1' if data[i] == data[i + 1] else '0' for i in range(0, len(data), 2)])
     return data
 
-data = dragon_cuve(input, disk_size)
+data = dragon_curve(input, disk_size)
 checksum_data = checksum(data)
 print(checksum_data)
