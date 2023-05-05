52f014edd58d3f13ce1fae51dd532738b3afb49e
Whoami354 <fabian.gregory98@hotmail.de>
2023-05-05 20:08:10 +0200

    Tag 15 gemacht

---
+++ Day_15/part02.py
@@ -0,0 +1,30 @@
+lines = open("input", "r", encoding='utf-8').read().strip().split('\n')
+
+discs = []
+positions = []
+for line in lines:
+    line = line.strip()
+    position = int(line.split()[3])
+    disc = int(line.split()[-1][:-1])
+    positions.append(position)
+    discs.append(disc)
+
+positions.append(11)
+discs.append(0)
+
+time = 0
+while True:
+    new = []
+    for idx, x in enumerate(discs):
+        value = (x + time + 1 + idx) % positions[idx]
+        new.append(value)
+
+    #print(new)
+    if len(set(new)) == 1:
+        break
+
+    time += 1
+
+print(time)
+
+
