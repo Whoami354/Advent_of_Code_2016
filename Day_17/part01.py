aefc7399b5afc15c55977397e135ae1e60ed04b1
Whoami354 <fabian.gregory98@hotmail.de>
2023-05-12 16:03:39 +0200

    Tag 17 gemacht

--- Day_17/part01.py
+++ Day_17/part01.py
@@ -1,75 +1,33 @@
-import hashlib
-input = "bwnlcvfs"
+from collections import deque
+from hashlib import md5
 
-#1. pos = oben
-#2. pos = unten
-#3. pos = links
-#4. pos = rechts
+INPUT = 'bwnlcvfs'
+VAULT = 3 + 3j
 
 
-openDoor = ['b', 'c', 'd', 'e', 'f']
-result = hashlib.md5(input.encode()).hexdigest()
-print(result)
-grid = [[' ' for _ in range(4)] for _ in range(4)]
-grid[0][0] = 'S'
-grid[3][3] = 'V'
-isTarget = 'V'
-newWay = input
-possibilties = {}
-ways = {0: 'U', 1: 'D', 2: 'L', 3: 'R'}
+def find_vault():
+    solutions = []
+    que = deque([(0 + 0j, '')])
 
-def check_S_Coordinate(grid):
-    for y in range(len(grid)):
-        for x in range(len(grid)):
-            if grid[y][x] == 'S':
-                return y,x
+    while que:
+        pos, path = que.popleft()
 
-def moving_S(letter, grid):
-    y,x = check_S_Coordinate(grid)
-    grid[y][x] = ' '
-    if letter == 'U':
-        y -= 1
-    if letter == 'D':
-        y += 1
-    if letter == 'L':
-        x += 1
-    if letter == 'R':
-        x -= 1
-    grid[y][x] = 'S'
-    return grid
+        if pos == VAULT:
+            solutions.append(path)
+            continue
 
-def setWalls(x: int, y: int, result):
-    # 0: 'U', 1: 'D', 2: 'L', 3: 'R'
-    if x == 0:
-        result = '#' + result[1:]
-    if x == 3:
-        result = result[:2] + '#' + result[3:]
-    if y == 0:
-        result = result[0] + '#' + result[2:]
-    if y == 3:
-        result = result[:1] + '#' + result[3:]
-    return result
+        u, d, l, r = md5((INPUT + path).encode()).hexdigest()[:4]
 
-while True:
-    y,x = check_S_Coordinate(grid)
-    print("Before", result)
-    newResult = setWalls(x,y,result)
-    print("After", result)
+        if u > 'a' and pos.imag > 0:
+            que.append((pos - 1j, path + 'U'))
+        if d > 'a' and pos.imag < 3:
+            que.append((pos + 1j, path + 'D'))
+        if l > 'a' and pos.real > 0:
+            que.append((pos - 1, path + 'L'))
+        if r > 'a' and pos.real < 3:
+            que.append((pos + 1, path + 'R'))
 
-    while not grid[3][3] == 'S':
-        y, x = check_S_Coordinate(grid)
-        newResult = setWalls(x, y, result)
-
-        for i in range(4):
-            if newResult[i] in openDoor:
-                newWay += (ways[i])
-                grid = moving_S(ways[i], grid)
-            # Das ziel wurde gefunden ? Überprüfen, ob der weg bereits gefunden wurde
-
-        result = hashlib.md5(newWay.encode()).hexdigest()
+    return solutions[0]
 
 
-
-
-
-
+print(find_vault())
--- Day_17/part02.py
+++ Day_17/part02.py
@@ -1,4 +1,33 @@
-input = "bwnlcvfs"
+from collections import deque
+from hashlib import md5
+
+INPUT = 'bwnlcvfs'
+VAULT = 3 + 3j
 
 
+def find_vault():
+    solutions = []
+    que = deque([(0 + 0j, '')])
 
+    while que:
+        pos, path = que.popleft()
+
+        if pos == VAULT:
+            solutions.append(path)
+            continue
+
+        u, d, l, r = md5((INPUT + path).encode()).hexdigest()[:4]
+
+        if u > 'a' and pos.imag > 0:
+            que.append((pos - 1j, path + 'U'))
+        if d > 'a' and pos.imag < 3:
+            que.append((pos + 1j, path + 'D'))
+        if l > 'a' and pos.real > 0:
+            que.append((pos - 1, path + 'L'))
+        if r > 'a' and pos.real < 3:
+            que.append((pos + 1, path + 'R'))
+
+    return len(solutions[-1])
+
+
+print(find_vault())
